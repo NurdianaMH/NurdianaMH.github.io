bf0e8d6a14a5283b14940d6f8eb555af5fa00fea
Veronika K <veronikakuzn99@gmail.com>
2019-10-22 11:20:39 +0300

    Added onSaveInstantState

--- app/src/main/java/com/example/calculateapp/MainActivity.java
+++ app/src/main/java/com/example/calculateapp/MainActivity.java
@@ -9,13 +9,44 @@ import com.example.calculateapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView exprBig;
-    public String toEvalExpression;
-    public String enteredExpression;
+    private TextView bigTextView, smallTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bigTextView = findViewById(R.id.textViewBig);
+        smallTextView = findViewById(R.id.textViewSmall);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+
+
+        savedInstanceState.putString("exprText", bigTextView.getText().toString());
+        savedInstanceState.putString("ansText", smallTextView.getText().toString());
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    //onRestoreInstanceState
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+
+
+        String expr = savedInstanceState.getString("exprText");
+        String ans = savedInstanceState.getString("ansText");
+        bigTextView.setText(expr);
+        smallTextView.setText(ans);
     }
 }
