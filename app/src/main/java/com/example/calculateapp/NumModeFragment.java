ff08142c3e53efda53d1db4b3aa06c424aac078b
Veronika K <veronikakuzn99@gmail.com>
2019-10-24 22:57:56 +0300

    Version 1.0

--- app/src/main/java/com/example/calculateapp/NumModeFragment.java
+++ app/src/main/java/com/example/calculateapp/NumModeFragment.java
@@ -18,21 +18,10 @@ import android.widget.TextView;
 import static java.lang.Double.NaN;
 import static java.lang.Double.isNaN;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NumModeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NumModeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NumModeFragment extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -47,15 +36,6 @@ public class NumModeFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NumModeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NumModeFragment newInstance(String param1, String param2) {
         NumModeFragment fragment = new NumModeFragment();
         Bundle args = new Bundle();
@@ -237,9 +217,15 @@ public class NumModeFragment extends Fragment implements View.OnClickListener {
                 exprTextView.setText(expr);
                 break;
             case R.id.buttonDelete:
-                String str = (String)exprTextView.getText();
-                str = str.substring(0, str.length() - 1);
-                exprTextView.setText(str);
+                String str = exprTextView.getText().toString();
+                if (!str.equals("")) {
+                    str = str.substring(0, str.length() - 1);
+                    exprTextView.setText(str);
+                    if (str.equals("")) {
+                        smallTextView.setText("");
+                    }
+                }
+
                 break;
 
             default:
@@ -248,22 +234,10 @@ public class NumModeFragment extends Fragment implements View.OnClickListener {
 
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
     }
 
     @Override
@@ -272,16 +246,7 @@ public class NumModeFragment extends Fragment implements View.OnClickListener {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
--- app/src/main/java/com/example/calculateapp/SciModeFragment.java
+++ app/src/main/java/com/example/calculateapp/SciModeFragment.java
@@ -15,21 +15,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SciModeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SciModeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SciModeFragment extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -43,15 +32,7 @@ public class SciModeFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SciFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static SciModeFragment newInstance(String param1, String param2) {
         SciModeFragment fragment = new SciModeFragment();
         Bundle args = new Bundle();
@@ -191,22 +172,9 @@ public class SciModeFragment extends Fragment implements View.OnClickListener {
 
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
     }
 
     @Override
@@ -215,16 +183,6 @@ public class SciModeFragment extends Fragment implements View.OnClickListener {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
